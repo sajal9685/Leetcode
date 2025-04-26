75f08e8a0918bb76d417698a801ccdfd2970d16f
Sajal Chaturvedi <sajalchaturvedi8@gmail.com>
2025-04-26 23:54:57 +0530

    .

--- Q_7.py
+++ Q_7.py
@@ -1,21 +1,29 @@
 # Input: x = 123
 # Output: 321
-x = 901000
+x = 120
 xStr = str(x)
 new = ""
+ch = 'F'
 for char in xStr:
     if char == "-":
         new = xStr.replace("-","",1)
         break
     elif char == "0":
         new = xStr[::-1].lstrip("0")
+        ch = 'T'
         break
     else:
         new = xStr
-xRev = new[::-1]          
-if x < 0:
-    xRev = "-" + xRev
-if xRev == "":
-         xRev = "0"      
+if ch=='F':        
+    xRev = new[::-1]          
+    if x < 0:
+        xRev = "-" + xRev
+    if xRev == "":
+        xRev = "0"
+    print(xRev)
+else:
+    if x < 0:
+        new = "-" + new
+    print(new)
+
   
-print(xRev)
