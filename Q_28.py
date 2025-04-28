1a36df4e36bebbb6212aeb3433841abeb4ee67de
Sajal Chaturvedi <sajalchaturvedi8@gmail.com>
2025-04-28 23:19:11 +0530

    Q_28

--- Q_28.py
+++ Q_28.py
@@ -2,16 +2,17 @@
 # Output: 0
 # Explanation: "sad" occurs at index 0 and 6.
 # The first occurrence is at index 0, so we return 0.
-haystack = "hello"
-needle = "ll"
+haystack = "a"
+needle = "a"
 li=[]
-for chr in range(1,len(haystack)-2):
+for chr in range(0,len(haystack)+1):
     for i in range(0,len(haystack)):
         pair=haystack[i:i+chr]
-        li.append(pair)
-for el in li:
+        li.append((pair, i))
+print(li)        
+for el ,idx in li:
     if el==needle:
-        print (0)
+        print (idx)
         break
 else:
     print(-1)   
